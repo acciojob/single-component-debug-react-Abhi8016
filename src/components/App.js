bfc0c6664867cf9c02f2bd6d7c591eb5c2e2d9a8
Abhinaba <abhi.roychoudhury.8@gmail.com>
2024-11-23 19:06:53 +0530

    trying 10

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,7 @@ const App = () => {
   return (
     <div id="main">
       <p>
-        Now I can render any React component on any DOM node I want using
-        ReactDOM.render
+      Now I can render any React component on any DOM node I want using ReactDOM.render
       </p>
     </div>
   );
